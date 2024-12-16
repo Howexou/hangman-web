b2afdb3a55a1c114d8de29f92896155203884b27
rguylan <guylan.richaud@ynov.com>
2024-12-16 11:18:03 +0100

    yo

--- scripts/struct.go
+++ scripts/struct.go
@@ -8,3 +8,9 @@ type Jeu struct {
 }
 
 var LeJeu Jeu
+
+/* Ici on a la structure du jeu
+Word est le mot a deviner
+Found est l'état des lettres à découvrir
+NbDeVie Bah c'est le nombre de vie
+DejaMis la liste de lettre tenté par le joueur*/
