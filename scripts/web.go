b837e3eb134e8b3c42f12a42308f453772261e1a
Howexou <l.cartagena1221@gmail.com>
2024-12-16 15:19:38 +0100

    fonction de win

--- scripts/web.go
+++ scripts/web.go
@@ -14,13 +14,14 @@ func Home(w http.ResponseWriter, r *http.Request) {
 func HandleRequests() {
 	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/play", Play)
+	http.HandleFunc("/win", Win)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("templates/static"))))
 	http.ListenAndServe(":8081", nil)
 }
 
 // Afficher le HTML avec des données "dynamiques"
 func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) { 
-	t, err := template.ParseFiles(Template)
+	t, err := template.ParseFiles(tmpl)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -41,3 +42,6 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func Win(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "victory", nil)
+}
