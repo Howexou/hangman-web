ebbb6c6d9ba6ea01cb805f92cb7da32d43b3cc71
rguylan <guylan.richaud@ynov.com>
2024-12-16 11:16:01 +0100

    yo

--- main.go
+++ main.go
@@ -6,18 +6,15 @@ import (
 )
 
 func main() {
-	// Charger les mots depuis le fichier words.txt
+	
 	h.LoadWords()
 
-	// Vérifie si des mots sont disponibles
 	if len(h.WordsList) == 0 {
 		fmt.Println("Erreur : Aucun mot trouvé dans le fichier words.txt.")
 		return
 	}
 
-	// Démarrer une partie
 	h.InitGame()
 
-	// Lancer le serveur web
 	h.HandleRequests()
 }
--- scripts/init.go
+++ scripts/init.go
@@ -8,8 +8,13 @@ import (
 	"strings"
 	"time"
 )
+/*La c'est les import, bufio et os pour la manip des des fichiers, 
+fmt pour l'affichage des messages
+math/rand et time pour choisir un mot aléatoirement
+strings pour les chaînes de carractéres*/
 
 var WordsList []string
+// On ajoute une variable WordsList ce sera pour stocker les les mots lus dans le fichier texte 
 
 func LoadWords() {
 	file, err := os.Open("DICTIONNAIRE/words.txt")
@@ -32,6 +37,13 @@ func LoadWords() {
 		}
 	}
 }
+/*La func LoadWords sert à ouvrir le fichier txt qui est words.txt
+os.Open("DICTIONNAIRE/wprds.txt") ouvre le fichier
+if err!= nil c'est si y a un problème
+defer file.Close() pour que le fichier soit toujours fermé
+scanner := bufio.NewScanner(file) sert a lire un un fichier ligne par ligne
+strings.TrimSpace , sa supprime les espaces*/
+
 
 func PickWord() string {
 	rand.Seed(time.Now().UnixNano())
@@ -40,6 +52,9 @@ func PickWord() string {
 	}
 	return WordsList[rand.Intn(len(WordsList))]
 }
+/* du coup ici sa prends un mot aléatoirement à partir de WordsList
+rand.Seed(time.Now().UnixNano()) permet d'initialiser le générateur de nombre aléatoire */
+
 
 
 func InitGame() {
@@ -61,6 +76,27 @@ func InitGame() {
 		DejaMis: []rune{},
 	}
 }
+/*la fonction InitGame permet d'initialiser une nouvelle partie
+WordsList qui sert toujours à choisir un mot aléatoire
+
+if word == "" {
+	fmt.Println("Erreur : Aucun mot disponible.")
+	return
+C'est pour si y a une erreur et y a un return pour relancer quand il y aura un mot valide
+
+hidden := ""
+for range word {
+	hidden += "_"
+}
+Pour créer une version masqué du mot ça
+
+LeJeu = Jeu{
+	Word:    word,
+	Found:   hidden,
+	NbDeVie: 8,
+	DejaMis: []rune{},
+}
+pour initialiser la partie, word le mot a deviner, found la version masqué du mot nbdevie le nombre de vie et dejamis un slice pour stocker les lettres déjà mise*/ 
 
 func ProcessLetter(letter rune) {
 	for _, l := range LeJeu.DejaMis {
@@ -87,3 +123,28 @@ func ProcessLetter(letter rune) {
 		LeJeu.NbDeVie--
 	}
 }
+
+/*LeJeu.Dejamis c'est pour les lettres déjà essayer
+for _, l := range LeJeu.DejaMis c'est une boucle qui parcourt les lettres déjà essayer
+et du coup si la lettre est déjà tenté sa mets le message suivant "Lettre déjà tenté"
+et on ajoute return, la lettre sera pas ajouté une deuxième fois
+LeJeu.DejaMis = append, append ajoute la lettre porposé
+
+found := false
+runes := []rune(LeJeu.Found)
+for i, ch := range LeJeu.Word {
+	if rune(ch) == letter {
+		runes[i] = letter
+		found = true
+	}
+}
+
+Sa vérifie si la lettre est dans le mot a deviner et pour que sa cherche la lettre dans le mot on a fait
+for i, ch := range LeJeu.Word , ch sa parcout les caractées
+
+
+if !found {
+	LeJeu.NbDeVie--
+}
+
+sa permet de réduire le nombre de vie*/
